9a96a17b8aa987bcd0a7315ea3ae66bfcaa93d45
Bao Duong <bduong11037@gmail.com>
2023-11-14 11:53:36 -0800

    Initial Commit

---
+++ app/scripts/src/ws-client.js
@@ -0,0 +1,38 @@
+let socket;
+
+function init(url) { 
+  socket = new WebSocket(url);
+  console.log('connecting...');
+}
+
+function registerOpenHandler(handlerFunction) {
+  socket.onopen = () => { 
+    console.log('open');
+    handlerFunction();
+  };
+}
+
+function registerMessageHandler(handlerFunction) { 
+  socket.onmessage = (e) => {
+    console.log('ws-client received message', e.data);
+    try { 
+      let data = JSON.parse(e.data);
+      handlerFunction(data);
+    } catch(error) { 
+      console.error('Error parsing json: ', error);
+    }
+  };
+}
+
+function sendMessage(payload) {
+  let str = JSON.stringify(payload);
+  console.log('ws-client sending stringified message', str);
+  socket.send(str);
+}
+
+export default { 
+  init,
+  registerOpenHandler,
+  registerMessageHandler,
+  sendMessage
+}
